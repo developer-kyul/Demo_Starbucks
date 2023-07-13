3d19cbc7ec7daa71a2881da1ff40756e1f0e8a04
Kyul <developer.kyul@gmail.com>
2023-07-14 06:31:56 +0900

    [style] 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,27 +1,3 @@
-/**
- * 검색창 작동
- */
-
-//검색창 요소 찾기
-const searchEl = document.querySelector('.search');
-const searchInputEl =searchEl.querySelector('input');
-
-//검색창 요소 클릭시에 실행
-searchEl.addEventListener('click', function(){
-    searchInputEl.focus();
-})
-
-//검색창 요소 내부 실제 input 요소에 포커스되면 실행되는 것
-searchInputEl.addEventListener('focus', function(){
-    searchEl.classList.add('focused')
-    searchInputEl.setAttribute('placeholder', '통합검색')
-})
-
-//검색창 요소에서 포커스 해제될 떄 실행되는 것
-searchInputEl.addEventListener('blur', function(){
-    searchEl.classList.remove('focused')
-    searchInputEl.setAttribute('placeholder', '')
-})
 
 /**
  * header 옆에 광고 배너
@@ -199,9 +175,3 @@ spyEls.forEach(function (spyEl) {
     .addTo(new ScrollMagic.Controller()) // 컨트롤러에 장면을 할당(필수!)
 })
 
-
-/**
- * 올해가 몇 년도인지 계산
- */
-const thisYear = document.querySelector('.this-year')
-thisYear.textContent = new Date().getFullYear()
